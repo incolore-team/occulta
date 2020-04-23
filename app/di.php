9eb45c8c2260d3385789b40bf943024b7eb7fab1
Pluveto <reg@pluvet.com>
2020-04-23 23:14:34 +0800

    first commit

--- app/di.php
+++ app/di.php
@@ -6,3 +6,4 @@ use App\Service\TemplateExtension;
 $themeDir = SYSTEM_USER_THEMES . "/" . Option::get("site.theme");
 Core::$view = \League\Plates\Engine::create($themeDir, 'php');
 Core::$view->register(new TemplateExtension());
+
